d90345c52e098fc6d94ee065a71eb338768632c2
krist7599555 <krist7599555@gmail.com>
2022-07-19 07:46:10 +0700

    feat: github page

---
+++ docs/precache-manifest.daad1addb61c35911900d25184d3efe2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0cd6d2b9add3072777a",
+    "url": "https://krist7599555.github.com/color2music/css/app.8e24ee66.css"
+  },
+  {
+    "revision": "1465a34826aed94d60b540aeffb0197a",
+    "url": "https://krist7599555.github.com/color2music/index.final.html"
+  },
+  {
+    "revision": "d3afaf9e973fc8b74f736408404b09a3",
+    "url": "https://krist7599555.github.com/color2music/index.html"
+  },
+  {
+    "revision": "e0cd6d2b9add3072777a",
+    "url": "https://krist7599555.github.com/color2music/js/app.a508a0e8.js"
+  },
+  {
+    "revision": "dbd4e41927d0564e15a3",
+    "url": "https://krist7599555.github.com/color2music/js/chunk-vendors.3f94f2b1.js"
+  },
+  {
+    "revision": "a231a0e5502f8f259e2b1d6b7b5146af",
+    "url": "https://krist7599555.github.com/color2music/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://krist7599555.github.com/color2music/robots.txt"
+  },
+  {
+    "revision": "aeaa7ae39a9b857205730b6e5186d4ce",
+    "url": "https://krist7599555.github.com/color2music/screenshot.png"
+  }
+]);
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+// vue.config.js
+// const { defineConfig } = require("@vue/cli-service");
+
+module.exports = {
+  // options...
+  publicPath: "https://krist7599555.github.com/color2music/",
+};
