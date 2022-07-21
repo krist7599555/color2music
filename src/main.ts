76776ec28813384f33494c5193e68e55c552068f
krist7599555 <krist7599555@gmail.com>
2022-07-21 14:02:51 +0700

    chore: remove unused

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,8 @@
+import "pepjs";
+import "jquery";
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
 
 createApp(App).mount("#app");
-// // import './registerServiceWorker'
-// import router from "./router";
-// import VueCompositionApi from "@vue/composition-api";
-
-// Vue.use(VueCompositionApi);
-
-import "pepjs"; // pointer jquery
-import "jquery"; // pointer jquery
-
-// // import Rx from 'rxjs/Rx'
-// // import VueRx from 'vue-rx'
-// // Vue.use(VueRx, Rx)
